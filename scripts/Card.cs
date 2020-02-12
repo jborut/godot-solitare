1009353769645f7875a8c4bcfd41d2aeff2b75ac
Borut Jegrisnik <borut.jegrisnik@instana.com>
2020-02-12 21:05:11 +0100

    add drag support

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -12,6 +12,7 @@ public class Card : Area2D
 	}
 	
 	private Sprite sprite;
+	private CollisionShape2D collisionObject;
 	private CardType type = CardType.Clubs;
 	private int number = 0;
 	
@@ -24,14 +25,23 @@ public class Card : Area2D
 
 	public float DrawWidth { get => Width * Scale.x; }
 	public float DrawHeight { get => Height * Scale.y; }
+
+	public bool IsMouseOver => isMouseInside;
+
+	private bool isMouseInside = false;
+	private bool isMousePressed = false;
+	private Vector2 dragMousePosition;
 	
 	public override void _Ready()
 	{
-		
 		AddToGroup(CardUtils.CardsGroupName);
 		sprite = GetNode<Sprite>("Sprite");
 		sprite.RegionEnabled = true;
 		sprite.RegionRect = GetRegion();
+
+		collisionObject = GetNode<CollisionShape2D>("CollisionShape");
+		((RectangleShape2D)collisionObject.Shape).Extents = new Vector2(Width / 2, Height / 2);
+		collisionObject.Position = new Vector2(0, 0);
 	}
 	
 	public void SetCardSprite(CardType type, int number)
@@ -39,6 +49,11 @@ public class Card : Area2D
 		this.type = type;
 		this.number = number;
 	}
+
+	private void OnMouseEntered() => isMouseInside = true;
+	private void OnMouseExited() => isMouseInside = false;
+
+	private bool IsDragged => isMouseInside && isMousePressed;
 	
 	private Rect2 GetRegion() => new Rect2(OffsetX + number * (Width + MarginWidth), OffsetY + (int)type * (Height + MarginHeight), Width, Height);
 }
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,54 +1,96 @@
-using System.Reflection;
 using Godot;
 using System;
 
 public class Main : Node2D
 {
-	[Export]
-	public Color BackgroundColor = ColorUtils.FromRGB(36, 117, 101);
-	
-	[Export]
-	public PackedScene Cards;
+  [Export]
+  public Color BackgroundColor = ColorUtils.FromRGB(36, 117, 101);
 
-	private Card[] deck;
-	
-	public override void _Ready()
-	{
-		VisualServer.SetDefaultClearColor(BackgroundColor);
-	}
+  [Export]
+  public PackedScene Cards;
 
-	private void OnHUDStartGame()
-	{
-		float offsetX = 0;
-		float offsetY = 0;
-		float fullWidth = 0;
+  private Card[] deck;
+  private Card selectedCard;
+	private int currentZindex = 64;
 
-		ClearDeck();
+  private bool isMousePressed = false;
+  private Vector2 mouseDragPosition = new Vector2(0, 0);
 
-		deck = CardUtils.GetRandomizedDeck(Cards);
+  public override void _Ready()
+  {
+    VisualServer.SetDefaultClearColor(BackgroundColor);
+  }
 
-		for (int i = 0; i < deck.Length; i++)
-		{
-				if (offsetX == 0)
-				{
-					offsetX = deck[i].DrawWidth / 2 + 10;
-					offsetY = deck[i].DrawHeight / 2 + 10;
+  public override void _Process(float delta)
+  {
+    if (isMousePressed)
+    {
+      if (selectedCard != null)
+      {
+        var currentPosition = GetViewport().GetMousePosition();
+        selectedCard.Position = selectedCard.Position - (mouseDragPosition - currentPosition);
+        mouseDragPosition = currentPosition;
+      }
+    }
+  }
 
-					fullWidth = 13 * (deck[i].DrawWidth + 10);
-				}
+  public override void _Input(InputEvent e)
+  {
+    if (e is InputEventMouseButton)
+    {
+      isMousePressed = ((InputEventMouseButton)e).IsPressed();
+      if (isMousePressed && deck != null)
+      {
+        foreach (var card in deck)
+        {
+          if (card.IsMouseOver)
+          {
+            selectedCard = card;
+            selectedCard.ZIndex = currentZindex++;
+            break;
+          }
+        }
+      }
+      else
+      {
+        selectedCard = null;
+      }
+      mouseDragPosition = GetViewport().GetMousePosition();
+    }
+  }
 
-				AddChild(deck[i]);
+  private void OnHUDStartGame()
+  {
+    float offsetX = 0;
+    float offsetY = 0;
+    float fullWidth = 0;
 
-				deck[i].Position = new Vector2(offsetX + i * (deck[i].DrawWidth + 10) - (i / 13) * fullWidth, offsetY + (i / 13) * (deck[i].DrawHeight + 10));
-				deck[i].Show();
-		}
-	}
-	
-	private void ClearDeck()
-	{
-		foreach (var node in GetTree().GetNodesInGroup(CardUtils.CardsGroupName))
-		{
-			((Node2D)node).QueueFree();
-		}
-	}
+    ClearDeck();
+
+    deck = CardUtils.GetRandomizedDeck(Cards);
+
+    for (int i = 0; i < deck.Length; i++)
+    {
+      if (offsetX == 0)
+      {
+        offsetX = deck[i].DrawWidth / 2 + 10;
+        offsetY = deck[i].DrawHeight / 2 + 10;
+
+        fullWidth = 13 * (deck[i].DrawWidth + 10);
+      }
+
+      AddChild(deck[i]);
+
+      deck[i].Position = new Vector2(offsetX + i * (deck[i].DrawWidth + 10) - (i / 13) * fullWidth, offsetY + (i / 13) * (deck[i].DrawHeight + 10));
+      deck[i].Show();
+    }
+  }
+
+  private void ClearDeck()
+  {
+    foreach (var node in GetTree().GetNodesInGroup(CardUtils.CardsGroupName))
+    {
+      ((Node2D)node).QueueFree();
+    }
+  }
 }
